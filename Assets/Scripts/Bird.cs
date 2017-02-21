980e1c7aad7db9c9003b95dcf12cfc3a4073ba3e
grin <dmitriy.grinevich.qa@gmail.com>
2017-02-21 23:19:40 +0200

    222

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,26 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    public float upFurce = 200f;
+
     private bool isDead = false;
     private Rigidbody2D rb2d;
+    private Animator anim;
 
-    // Use this for initialization
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (isDead == false)
         {
             if (Input.GetMouseButtonDown(0))
             {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upFurce));
+                anim.SetTrigger("Flap");
             }
         }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        isDead = true;
+        anim.SetTrigger("Die");
     }
 }
