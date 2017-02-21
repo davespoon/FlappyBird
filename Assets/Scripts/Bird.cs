9a0f3418f29b2e0696e69d7433719b7a80ef5cda
grin <dmitriy.grinevich.qa@gmail.com>
2017-02-21 20:46:44 +0200

    111

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+            }
+        }
+    }
+}
